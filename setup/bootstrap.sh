cf82e1b2aad57a823eb3fd93364c5bfc566386b7
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2021-03-19 09:28:23 +0100

    fixed some shellcheck warnings

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -11,85 +11,85 @@ stty "$stty_orig"                        # restore terminal setting.
 current_user=$(whoami)
 
 # enable colored output for pacman
-echo $passwd | sudo -S sed -i '/Color/s/^#//g' /etc/pacman.conf
+echo "$passwd" | sudo -S sed -i '/Color/s/^#//g' /etc/pacman.conf
 
 # sync mirrors, update the system
-echo $passwd | sudo -S pacman -Syyu
+echo "$passwd" | sudo -S pacman -Syyu
 
 # install git
-echo $passwd | sudo -S pacman -S --noconfirm git
+echo "$passwd" | sudo -S pacman -S --noconfirm git
 
 # installing paru
-git clone https://aur.archlinux.org/paru.git $HOME/source/paru
-cd $HOME/source/paru
+git clone https://aur.archlinux.org/paru.git "$HOME/source/paru"
+cd "$HOME"/source/paru || exit
 makepkg -si
-cd
+cd || exit
 
 # install every package from packagelist
 cat ./packagelist | xargs paru --sudoloop -S --noconfirm
 
 # installing pynvim
-echo $passwd | sudo -S -u $current_user python3 -m pip install --user --upgrade pynvim
+echo "$passwd" | sudo -S -u "$current_user" python3 -m pip install --user --upgrade pynvim
 
 # nextdns settings
-echo $passwd | sudo -S nextdns install -config 51a3bd -report-client-info -auto-activate
+echo "$passwd" | sudo -S nextdns install -config 51a3bd -report-client-info -auto-activate
 
 # virt-manager
-echo $passwd | sudo -S usermod -aG libvirt $current_user
-echo $passwd | sudo -S systemctl enable libvirtd
-echo $passwd | sudo -S virsh net-autostart default
+echo "$passwd" | sudo -S usermod -aG libvirt "$current_user"
+echo "$passwd" | sudo -S systemctl enable libvirtd
+echo "$passwd" | sudo -S virsh net-autostart default
 
 # printing service
-echo $passwd | sudo -S systemctl enable org.cups.cupsd.socket
+echo "$passwd" | sudo -S systemctl enable org.cups.cupsd.socket
 
 # firewall
-echo $passwd | sudo -S ufw default deny incoming
-echo $passwd | sudo -S ufw default allow outgoing
-echo $passwd | sudo -S systemctl enable ufw
-echo $passwd | sudo -S ufw enable
+echo "$passwd" | sudo -S ufw default deny incoming
+echo "$passwd" | sudo -S ufw default allow outgoing
+echo "$passwd" | sudo -S systemctl enable ufw
+echo "$passwd" | sudo -S ufw enable
 
 # windscribe vpn service
-echo $passwd | sudo -S systemctl enable windscribe
+echo "$passwd" | sudo -S systemctl enable windscribe
 
 # power saving service
-echo $passwd | sudo -S systemctl enable tlp
+echo "$passwd" | sudo -S systemctl enable tlp
 
 # cloning my configs from github to a bare repository for config file management
-git clone --bare https://github.com/laszloszurok/config $HOME/.cfg
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
+git clone --bare https://github.com/laszloszurok/config "$HOME"/.cfg
+/usr/bin/git --git-dir="$HOME"/.cfg/ --work-tree="$HOME" config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir="$HOME"/.cfg/ --work-tree="$HOME" checkout -f
 
 # cloning my scripts
-git clone https://github.com/laszloszurok/scripts $HOME/source/scripts
+git clone https://github.com/laszloszurok/scripts "$HOME"/source/scripts
 
 # cloning my suckless builds
 suckless_dir="$HOME/source/suckless-builds"
-git clone https://github.com/laszloszurok/dwm.git $suckless_dir/dwm
-git clone https://github.com/laszloszurok/dwmblocks.git $suckless_dir/dwmblocks
-git clone https://github.com/laszloszurok/dmenu.git $suckless_dir/dmenu
-git clone https://github.com/laszloszurok/st.git $suckless_dir/st
-git clone https://github.com/laszloszurok/slock.git $suckless_dir/slock
-git clone https://github.com/laszloszurok/wmname.git $suckless_dir/wmname
+git clone https://github.com/laszloszurok/dwm.git "$suckless_dir"/dwm
+git clone https://github.com/laszloszurok/dwmblocks.git "$suckless_dir"/dwmblocks
+git clone https://github.com/laszloszurok/dmenu.git "$suckless_dir"/dmenu
+git clone https://github.com/laszloszurok/st.git "$suckless_dir"/st
+git clone https://github.com/laszloszurok/slock.git "$suckless_dir"/slock
+git clone https://github.com/laszloszurok/wmname.git "$suckless_dir"/wmname
 
 # installing my suckless builds
-cd $suckless_dir
-for d in $suckless_dir/*/ ; do
-    cd "$d"
-    echo $passwd | sudo -S make install
+cd "$suckless_dir" || exit
+for d in "$suckless_dir"/*/ ; do
+    cd "$d" || exit
+    echo "$passwd" | sudo -S make install
     cd ..
 done
-cd
+cd || exit
 
 # cloning my wallpapers
-git clone https://github.com/laszloszurok/Wallpapers $HOME/pictures/wallpapers
+git clone https://github.com/laszloszurok/Wallpapers "$HOME"/pictures/wallpapers
 
 # installing gtk palenight theme
-git clone https://github.com/jaxwilko/gtk-theme-framework.git $HOME/source/palenight-gtk
-cd $HOME/source/palenight-gtk
+git clone https://github.com/jaxwilko/gtk-theme-framework.git "$HOME"/source/palenight-gtk
+cd "$HOME"/source/palenight-gtk || exit
 ./main.sh -i -o
 
 # global gtk-2 settings
-echo $passwd | sudo -S tee /etc/gtk-2.0/gtkrc <<< "gtk-theme-name=\"palenight\"
+echo "$passwd" | sudo -S tee /etc/gtk-2.0/gtkrc <<< "gtk-theme-name=\"palenight\"
 gtk-icon-theme-name=\"palenight\"
 gtk-font-name=\"Sans 9\"
 gtk-cursor-theme-name=\"palenight\"
@@ -105,7 +105,7 @@ gtk-xft-hinting=1
 gtk-xft-hintstyle=\"hintfull\""
 
 # global gtk-3 settings
-echo $passwd | sudo -S tee /etc/gtk-3.0/settings.ini <<< "[Settings]
+echo "$passwd" | sudo -S tee /etc/gtk-3.0/settings.ini <<< "[Settings]
 gtk-theme-name=palenight
 gtk-icon-theme-name=palenight
 gtk-font-name=Sans 9
@@ -123,23 +123,23 @@ gtk-xft-hintstyle=hintfull
 gtk-application-prefer-dark-theme=true"
 
 # unify gtk and qt themes
-echo $passwd | sudo -S tee -a /etc/environment <<< "QT_QPA_PLATFORMTHEME=gtk2"
+echo "$passwd" | sudo -S tee -a /etc/environment <<< "QT_QPA_PLATFORMTHEME=gtk2"
 
 # spotify wm
-git clone https://github.com/dasJ/spotifywm.git $HOME/.config/spotifywm
-cd $HOME/.config/spotifywm
+git clone https://github.com/dasJ/spotifywm.git "$HOME"/.config/spotifywm
+cd "$HOME"/.config/spotifywm || exit
 make
-echo $passwd | sudo -S -u $current_user tee /usr/local/bin/spotify <<< "LD_PRELOAD=/usr/lib/libcurl.so.4:$HOME/.config/spotifywm/spotifywm.so /usr/bin/spotify"
-echo $passwd | sudo -S chmod +x /usr/local/spotify
+echo "$passwd" | sudo -S -u "$current_user" tee /usr/local/bin/spotify <<< "LD_PRELOAD=/usr/lib/libcurl.so.4:$HOME/.config/spotifywm/spotifywm.so /usr/bin/spotify"
+echo "$passwd" | sudo -S chmod +x /usr/local/spotify
 
-cd
+cd || exit
 
 # changing the default shell to zsh
-echo $passwd | sudo -S tee /etc/zsh/zshenv <<< "ZDOTDIR=\$HOME/.config/zsh"
+echo "$passwd" | sudo -S tee /etc/zsh/zshenv <<< "ZDOTDIR=\$HOME/.config/zsh"
 chsh -s /usr/bin/zsh
 
-echo $passwd | sudo -S mkdir /usr/share/xsessions
-echo $passwd | sudo -S tee /usr/share/xsessions/dwm.desktop <<< "[Desktop Entry]
+echo "$passwd" | sudo -S mkdir /usr/share/xsessions
+echo "$passwd" | sudo -S tee /usr/share/xsessions/dwm.desktop <<< "[Desktop Entry]
 Encoding=UTF-8
 Name=dwm
 Comment=Dynamic Window Manager
@@ -147,7 +147,7 @@ Exec=/usr/local/bin/dwm
 Type=Application"
 
 # touchpad settings
-echo $passwd | sudo -S tee /etc/X11/xorg.conf.d/30-touchpad.conf <<< "Section \"InputClass\"
+echo "$passwd" | sudo -S tee /etc/X11/xorg.conf.d/30-touchpad.conf <<< "Section \"InputClass\"
     Identifier \"touchpad\"
     Driver \"libinput\"
     MatchIsTouchpad \"on\"
@@ -156,14 +156,14 @@ echo $passwd | sudo -S tee /etc/X11/xorg.conf.d/30-touchpad.conf <<< "Section \"
 EndSection"
 
 # automatically spin down the secondary hdd in my machine if it is not in use
-echo $passwd | sudo -S tee /usr/lib/systemd/system-sleep/hdparm <<< "#!/bin/sh
+echo "$passwd" | sudo -S tee /usr/lib/systemd/system-sleep/hdparm <<< "#!/bin/sh
 
 case \$1 in post)
         /usr/bin/hdparm -q -S 60 -y /dev/sda
         ;;
 esac"
 
-echo $passwd | sudo -S tee /etc/systemd/system/hdparm.service <<< "[Unit]
+echo "$passwd" | sudo -S tee /etc/systemd/system/hdparm.service <<< "[Unit]
 Description=hdparm sleep
 
 [Service]
@@ -175,10 +175,10 @@ WantedBy=multi-user.target"
 ################################################################################
 
 # disable hardware bell on boot
-echo $passwd | sudo -S tee /etc/modprobe.d/pcspkr-blacklist.conf <<< "blacklist pcspkr"
+echo "$passwd" | sudo -S tee /etc/modprobe.d/pcspkr-blacklist.conf <<< "blacklist pcspkr"
 
 # service to launch slock on suspend
-echo $passwd | sudo -S tee /etc/systemd/system/slock@.service <<< "[Unit]
+echo "$passwd" | sudo -S tee /etc/systemd/system/slock@.service <<< "[Unit]
 Description=Lock X session using slock for user %i
 Before=sleep.target
 Before=suspend.target
@@ -195,19 +195,19 @@ TimeoutSec=infinity
 WantedBy=sleep.target
 WantedBy=suspend.target"
 
-echo $passwd | sudo -S systemctl enable slock@$current_user.service
+echo "$passwd" | sudo -S systemctl enable "slock@$current_user.service"
 
 # disable tty swithcing when X is running, so the lockscreen cannot be bypassed
-echo $passwd | sudo -S tee /etc/X11/xorg.conf.d/xorg.conf <<< "Section \"ServerFlags\"
+echo "$passwd" | sudo -S tee /etc/X11/xorg.conf.d/xorg.conf <<< "Section \"ServerFlags\"
     Option \"DontVTSwitch\" \"True\"
 EndSection"
 
 # cron service
-echo $passwd | sudo -S systemctl enable cronie.service
-crontab $HOME/.config/cronjobs
+echo "$passwd" | sudo -S systemctl enable cronie.service
+crontab "$HOME"/.config/cronjobs
 
 # udev rule to allow users in the "video" group to set the display brightness
-echo $passwd | sudo -S tee /etc/udev/rules.d/90-backlight.rules <<< "SUBSYSTEM==\"backlight\", ACTION==\"add\", \
+echo "$passwd" | sudo -S tee /etc/udev/rules.d/90-backlight.rules <<< "SUBSYSTEM==\"backlight\", ACTION==\"add\", \
   RUN+=\"/bin/chgrp video /sys/class/backlight/%k/brightness\", \
   RUN+=\"/bin/chmod g+w /sys/class/backlight/%k/brightness\""
 
