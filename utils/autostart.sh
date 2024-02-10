75a804a689345880b8c5e3f89a73f0ad0240c9e8
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-02-10 10:28:28 +0100

    use physlock for locking the screen

--- utils/autostart.sh
+++ utils/autostart.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-#lockcmd="swaylock -f"
-lockcmd="gtklock \
-    --style ~/.config/gtklock/config.css \
-    --start-hidden \
-    --idle-hide \
-    --idle-timeout 10 \
-    --daemonize"
+# to be able to suspend the system after a session gets locked
+# with physlock the following polkit rules are needed in the
+# file /etc/polkit-1/rules.d/10-power.rules:
+# polkit.addRule(function(action, subject) {
+# if ((action.id == "org.freedesktop.login1.suspend" ||
+#     action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+#     action.id == "org.freedesktop.login1.hibernate" ||
+#     action.id == "org.freedesktop.login1.hibernate-multiple-sessions" ||
+#     action.id == "org.freedesktop.login1.reboot" ||
+#     action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+#     action.id == "org.freedesktop.login1.power-off" ||
+#     action.id == "org.freedesktop.login1.power-off-multiple-sessions") &&
+#     (subject.isInGroup("power"))) {
+#     return polkit.Result.YES;
+# }
+# });
+# and the user has to be in the power gorup
+
+# shellcheck disable=SC2016
+lockcmd='[ -z "$(pidof physlock)" ] && physlock -s -m -p "Session locked" -d'
 
 # run wayland stuff
 
@@ -20,11 +33,16 @@ waybar &
 
 swaybg --image ~/pictures/wallpapers/planets-2.jpg &
 
+# swayidle -w \
+#     timeout 420 'hyprctl dispatch dpms off' \
+#     timeout 600 "$lockcmd" \
+#     timeout 2100 'systemctl suspend' \
+#     resume 'hyprctl dispatch dpms on' \
+#     before-sleep "$lockcmd" &
+
 swayidle -w \
-    timeout 420 'hyprctl dispatch dpms off' \
     timeout 600 "$lockcmd" \
     timeout 2100 'systemctl suspend' \
-    resume 'hyprctl dispatch dpms on' \
     before-sleep "$lockcmd" &
 
 battery-low.sh &
