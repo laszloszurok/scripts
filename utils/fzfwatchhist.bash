bb473119dd4720c31d5efee334dd0b64a7647bd3
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-02-11 14:10:42 +0100

    add fzfwatchhist.bash

---
+++ utils/fzfwatchhist.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+fzfwrap() {
+    sel="$(tac "$HOME/.config/mpv/history.log" | uniq | fzf --no-preview | awk '{print $NF}')"
+    if [ -n "$sel" ]; then
+        notify-send "Loading..."
+        setsid -f mpv "$sel" || notify-send "Failed to load video"
+    fi
+}
+
+export -f fzfwrap
+
+alacritty \
+    --class float \
+    --command bash -c fzfwrap
