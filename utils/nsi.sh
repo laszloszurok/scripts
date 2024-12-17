e8904ba96ea9dd92032ae0091c02e62fc73d960c
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-12-17 20:34:41 +0100

    add neovim wrapper script

---
+++ utils/nsi.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# neovim single instance
+
+socket=~/.cache/nvim/server.sock
+
+if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+    nvim "$@"
+elif echo "$@" | grep vifm.rename; then
+    nvim "$@"
+else
+    if [ -S "$socket" ]; then
+        nvim --server "$socket" --remote-send ":cd $PWD<cr>" > /dev/null 2>&1
+        nvim --server "$socket" --remote "$@" > /dev/null 2>&1
+        if [ -n "$WAYLAND_DISPLAY" ]; then
+            hyprctl dispatch focuswindow "address:$(hyprctl clients -j | jq -r '.[] | select(.class=="neovim-single-instance").address')"
+        fi
+    else
+        setsid -f kitty --class neovim-single-instance nvim "$@" --listen "$socket" > /dev/null 2>&1
+    fi
+fi
