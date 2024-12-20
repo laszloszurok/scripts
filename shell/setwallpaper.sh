6d67d22b3dfc9a00e867c449fd863fcd3f05455c
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-12-20 20:41:17 +0100

    add wayland support to the wallpaper script

--- shell/setwallpaper.sh
+++ shell/setwallpaper.sh
@@ -3,6 +3,16 @@
 # If this script is given an image, it sets that image as the wallpaper.
 # Otherwise it sets the wallpaper to the last image that was used.
 
-wallpaper=$HOME/.config/wallpaper
-[ -f "$1" ] && ln -sf "$(readlink -f "$1")" "$wallpaper" && notify-send -i "$wallpaper" "Changing wallpaper..."
-xwallpaper --zoom "$wallpaper"
+wallpaper_path=$HOME/.config/wallpaper
+
+if [ -f "$1" ]; then
+    notify-send -i "$wallpaper_path" "Changing wallpaper..."
+    ln -sf "$(readlink -f "$1")" "$wallpaper_path"
+fi
+
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    xwallpaper --zoom "$wallpaper_path"
+elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    killall hyprpaper
+    setsid -f hyprpaper > /dev/null 2>&1
+fi
