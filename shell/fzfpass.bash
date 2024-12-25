1029ba4be01ec3b39be5046201a0da3c0878da8d
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-12-26 00:05:30 +0100

    convert fzfpass from bash to posix

--- shell/fzfpass.bash
+++ shell/fzfpass.bash
@@ -1,20 +1,13 @@
-#!/usr/bin/env bash
-
-usage() {
-    echo "Usage:
-  fzfpass.bash [OPTIONS]
-
-Options:
-  -h, --help - Display this message
-  -s, --show - Only show the selected item instead of copying it
-  --height HEIGHT - set the height of the fzf UI to HEIGHT
-"
-}
+#!/bin/sh
 
 fzfparams="--scheme=history --no-preview"
+cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/fzfpass"
+recent_cache="$cache_dir/recent"
+all_cache="$cache_dir/all"
+mkdir -p "$cache_dir"
 
+# process commandline options
 eval set -- "$(getopt --options 'hs' --long 'help,show,height:' --name 'fzfpass.bash' -- "$@")"
-
 while true; do
 	case "$1" in
 		'-h'|'--help')
@@ -42,35 +35,37 @@ while true; do
 	esac
 done
 
-shopt -s nullglob globstar
+usage() {
+    echo "Usage:
+  fzfpass.bash [OPTIONS]
 
-# files to work with
-cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/fzfpass"
-recent_cache="$cache_dir/recent"
-all_cache="$cache_dir/all"
-
-mkdir -p "$cache_dir"
-
-# Insert the picked entry to the first line of the cache file
-# and remove any other occurrences. This way the last picked
-# entry will always be the first in the list.
-update_cache() {
-    echo -e "$1\n$(sed "\|$1|d" "$recent_cache")" > "$recent_cache"
+Options:
+  -h, --help - Display this message
+  -s, --show - Only show the selected item instead of copying it
+  --height HEIGHT - set the height of the fzf UI to HEIGHT
+"
 }
-# merging $recent and $all, find the first occurrence of every entry
-# and remove any other occurrences
+
+# Write the selected item to the first line of the cache file
+# and remove any other occurrences. This way the last picked
+# item will always be the first in the list.
+update_cache() {
+    printf '%s\n%s\n' "$1" "$(sed "\|$1|d" "$recent_cache")" > "$recent_cache"
+}
+
+# get all the names of the entries in the password-store without the .gpg extension
+IFS=$(printf '\n')
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
-password_files=( "$prefix"/**/*.gpg )
-password_files=( "${password_files[@]#"$prefix"/}" )
-password_files=( "${password_files[@]%.gpg}" )
+password_files=$(find "$prefix" -name "*.gpg" | sed -e "s|$prefix/||g" -e 's|\.gpg$||')
+unset IFS
 
 # write to recent_cache if it does not exist
-[ ! -f "$recent_cache" ] && echo "${password_files[@]}" | tr " " "\n" > "$recent_cache"
+[ ! -f "$recent_cache" ] && echo "$password_files" > "$recent_cache"
 
 # always write the full list to all_cache, so the list will be always up to date
-echo "${password_files[@]}" | tr " " "\n" > "$all_cache"
+echo "$password_files" > "$all_cache"
 
-# remove anything from recent_cache that is not present in all_cache (eg. the user deleted an entry from the password-store)
+# remove anything from recent_cache that is not present in all_cache (eg. the user deleted an item from the password-store)
 grep -vxf "$all_cache" "$recent_cache" | while read -r line; do
     sed -i "\|$line|d" "$recent_cache"
 done
@@ -80,7 +75,7 @@ list=$(awk '!visited[$0]++' "$recent_cache" "$all_cache")
 # shellcheck disable=SC2086
 pass_sel="$(printf '%s\n' "$list" | fzf $fzfparams)"
 
-if [[ -n "$pass_sel" ]] && [[ "${password_files[*]}" =~ $pass_sel ]]; then
+if [ -n "$pass_sel" ] && echo "$password_files" | grep --silent "$pass_sel"; then
     if [ "$show" = "true" ]; then
         echo "$pass_sel"
         pass show "$pass_sel"
@@ -90,5 +85,3 @@ if [[ -n "$pass_sel" ]] && [[ "${password_files[*]}" =~ $pass_sel ]]; then
     fi
     update_cache "$pass_sel"
 fi
-
-[ "$1" = "--help" ] || [ "$1" = "-h" ] && usage && exit 0
