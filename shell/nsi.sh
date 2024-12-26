8507ac5e9a0e9525696ff5dd06505844c9e5ef89
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-12-26 13:38:24 +0100

    make sure to switch to normal mode when opening a new file

--- shell/nsi.sh
+++ shell/nsi.sh
@@ -10,7 +10,7 @@ elif echo "$@" | grep vifm.rename; then
     nvim "$@"
 else
     if [ -S "$socket" ]; then
-        nvim --server "$socket" --remote-send ":cd $PWD<cr>" > /dev/null 2>&1
+        nvim --server "$socket" --remote-send "<esc><esc>:cd $PWD<cr>" > /dev/null 2>&1
         nvim --server "$socket" --remote "$@" > /dev/null 2>&1
         if [ -n "$WAYLAND_DISPLAY" ]; then
             hyprctl dispatch focuswindow "address:$(hyprctl clients -j | jq -r '.[] | select(.class=="neovim-single-instance").address')"
