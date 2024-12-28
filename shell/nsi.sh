0806fb2900561f4cab7fe75ae9340a879998d83c
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-12-28 14:14:27 +0100

    fix hyprctl error under qtile

--- shell/nsi.sh
+++ shell/nsi.sh
@@ -13,7 +13,9 @@ else
         nvim --server "$socket" --remote-send "<esc><esc>:cd $PWD<cr>" > /dev/null 2>&1
         nvim --server "$socket" --remote "$@" > /dev/null 2>&1
         if [ -n "$WAYLAND_DISPLAY" ]; then
-            hyprctl dispatch focuswindow "address:$(hyprctl clients -j | jq -r '.[] | select(.class=="neovim-single-instance").address')"
+            if [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]; then
+                hyprctl dispatch focuswindow "address:$(hyprctl clients -j | jq -r '.[] | select(.class=="neovim-single-instance").address')"
+            fi
         fi
     else
         setsid -f kitty --class neovim-single-instance nvim "$@" --listen "$socket" > /dev/null 2>&1
