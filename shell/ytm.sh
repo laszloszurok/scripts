2ab7bf0cfb80eb401d91fa930098af2bc88655a8
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2024-12-30 20:12:07 +0100

    add ytm.sh

---
+++ shell/ytm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# launch youtube music in a separate qutebrowser session
+
+set -- "${@}" --basedir ~/.local/share/qutebrowser-ytm
+set -- "${@}" --config-py ~/.config/qutebrowser/config.py
+set -- "${@}" --target window
+set -- "${@}" --set tabs.show never
+set -- "${@}" --set statusbar.show in-mode
+set -- "${@}" --set window.title_format youtube-music
+
+qutebrowser music.youtube.com "${@}"
