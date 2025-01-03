65ad737ef7a8cf9b6096f09ee4975b836264ff87
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2025-01-03 20:48:24 +0100

    disable session save

--- shell/ytm.sh
+++ shell/ytm.sh
@@ -8,5 +8,6 @@ set -- "${@}" --target window
 set -- "${@}" --set tabs.show never
 set -- "${@}" --set statusbar.show in-mode
 set -- "${@}" --set window.title_format youtube-music
+set -- "${@}" --set auto_save.session false
 
 qutebrowser music.youtube.com "${@}"
