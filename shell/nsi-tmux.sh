f67e662581a57f279b762099ece63e2abde17b0a
Szurok László <laszlo.szurok@idomsoft.hu>
2025-03-28 14:57:14 +0100

    add nsi-tmux.sh

---
+++ shell/nsi-tmux.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# neovim single instance in tmux
+
+socket=~/.cache/nvim/server.sock
+
+if ! pidof tmux; then
+    nvim "$@"
+elif echo "$@" | grep vifm.rename; then
+    nvim "$@"
+else
+    if [ -S "$socket" ]; then
+        nvim --server "$socket" --remote-send "<esc><esc>:cd $PWD<cr>" > /dev/null 2>&1
+        nvim --server "$socket" --remote "$@" > /dev/null 2>&1
+        tmux select-window -t neovim
+    else
+        tmux send-keys -t neovim.0 "cd $PWD && nvim '$*' --listen '$socket'" Enter
+        tmux select-window -t neovim
+    fi
+fi
