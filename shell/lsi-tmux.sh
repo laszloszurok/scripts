4c86db8a5dff32c6d5f9680b33978da9b0ad0fee
Szurok László <laszlo.szurok@idomsoft.hu>
2025-04-02 13:12:58 +0200

    add sli-tmux.sh

---
+++ shell/lsi-tmux.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# lazygit single instance in tmux
+
+if [ ! "$TERM" = "tmux-256color" ] || [ -z "$TMUX" ]; then
+    lazygit "$@"
+else
+    killall lazygit
+    tmux select-window -t lazygit
+    tmux send-keys -t lazygit.0 "cd $PWD && lazygit $*" Enter
+fi
