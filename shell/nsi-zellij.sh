b600fe4187898c1dec375e0a87255098eb5aac3b
Szurok László <laszlo.szurok@idomsoft.hu>
2025-04-15 15:49:52 +0200

    fix startup when socket exists

--- shell/nsi-zellij.sh
+++ shell/nsi-zellij.sh
@@ -4,18 +4,31 @@
 
 socket=~/.cache/nvim/server.sock
 
+nvim_start_listen() {
+    zellij action go-to-tab-name neovim
+    zellij action write-chars "cd $PWD && nvim $* --listen '$socket'"
+    zellij action write 13
+}
+
+nvim_attach() {
+    nvim --server "$socket" --remote-send "<esc><esc>:cd $PWD<cr>" > /dev/null 2>&1
+    nvim --server "$socket" --remote "$@" > /dev/null 2>&1
+    zellij action go-to-tab-name neovim
+}
+
 if [ -z "$ZELLIJ_SESSION_NAME" ]; then
     nvim "$@"
 elif echo "$@" | grep vifm.rename; then
     nvim "$@"
 else
     if [ -S "$socket" ]; then
-        nvim --server "$socket" --remote-send "<esc><esc>:cd $PWD<cr>" > /dev/null 2>&1
-        nvim --server "$socket" --remote "$@" > /dev/null 2>&1
-        zellij action go-to-tab-name neovim
+        if pgrep --full 'nvim .* --listen'; then
+            nvim_attach "$@"
+        else
+            rm -f "$socket"
+            nvim_start_listen "$@"
+        fi
     else
-        zellij action go-to-tab-name neovim
-        zellij action write-chars "cd $PWD && nvim $* --listen '$socket'"
-        zellij action write 13
+        nvim_start_listen "$@"
     fi
 fi
